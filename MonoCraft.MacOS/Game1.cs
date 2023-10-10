71279d1fa106a838689a53a4d956ded81c4c50dd
Oktay <oktay-georg.trippel@telekom.de>
2023-10-10 14:34:14 +0200

    yeeeeeeeeeeeeaaaaaaa

---
+++ MonoCraft.MacOS/Game1.cs
@@ -0,0 +1,114 @@
+using ConsoleClient;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace MonoCraft.MacOS
+{
+
+    public struct Block
+    {
+        public int X, Y, Z;
+        public int Id;
+    }
+
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private SpriteFont _font;
+
+        NetClient client = new NetClient();
+
+        private List<Block> _blocks = new();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            client.ConnectAsync("192.168.178.63", 25565);
+            PacketHandler.OnBlockChange += UpdateBlock;
+            IsMouseVisible = true;
+        }
+
+        private void UpdateBlock(object sender, BlockChangeEventArgs e)
+        {
+
+            Console.WriteLine(e.ToString());
+
+            int x = e.X;
+            int y = e.Y;
+            int z = e.Z;
+            int id = e.Id;
+
+            for (int i = _blocks.Count - 1; i >= 0; i--)
+            {
+                Block b = _blocks[i];
+                if (b.X == x && b.Y == y && b.Z == z)
+                {
+                    _blocks.Remove(b);
+                }
+            }
+
+            _blocks.Add(new Block()
+            {
+                X = x,
+                Y = y,
+                Z = z,
+                Id = id
+            });
+
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _font = Content.Load<SpriteFont>("font");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            for (int i = _blocks.Count - 1; i >= 0; i--)
+            {
+                Block block = _blocks[i];
+                int x = block.X;
+                int y = block.Y;
+                int z = block.Z;
+                int id = block.Id;
+                if (id == 0)
+                {
+                    continue;
+                }
+                _spriteBatch.DrawString(_font, $"{id}", new Vector2(x, z) * 16, Color.White);
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoCraft.MacOS/NetClient.cs
@@ -0,0 +1,356 @@
+using MonoCraft.Net;
+using System.Collections.Concurrent;
+using System.Data;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleClient
+{
+    class Player
+    {
+        public double X, Y, Z;
+        public double VelX, VelZ;
+        public float Yaw, Pitch;
+    }
+
+    class PacketStream
+    {
+        public int PacketLength;
+        public byte[] Data;
+    }
+
+    internal class NetClient
+    {
+        private Thread _readThread;
+        private Thread _sendThread;
+        private Thread _processThread;
+
+        private Socket _socket;
+        private NetworkStream _networkStream;
+
+        private ushort _port;
+        private string _address;
+
+        private IPAddress _ipAddress;
+        private IPEndPoint _ipEndPoint;
+
+        public bool IsConnected { get; private set; }
+        public Action OnConnectionEstablished;
+        public Action OnServerTick;
+        public int CompressionThreshold = -1;
+
+        public ConcurrentQueue<MemoryStream> OutQueue { get; private set; } = new();
+        public ConcurrentQueue<PacketStream> InQueue { get; private set; } = new();
+
+        public Player Player;
+
+        private Semaphore _packetsToProcess = new Semaphore(0, 1);
+        public object penis;
+
+        public NetClient()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            OnConnectionEstablished += Init;
+        }
+
+        public async Task ConnectAsync(string address, ushort port)
+        {
+            _address = address;
+            _port = port;
+            //_ipAddress = IPAddress.Parse(_address);
+            //_ipEndPoint = new IPEndPoint(_ipAddress, _port);
+
+            await _socket.ConnectAsync(_address, _port);
+
+            if (_socket.Connected)
+            {
+                IsConnected = true;
+                _networkStream = new NetworkStream(_socket, true);
+                OnConnectionEstablished?.Invoke();
+            }
+        }
+
+        public async Task DisconnectAsync()
+        {
+            await _socket.DisconnectAsync(false);
+            IsConnected = false;
+        }
+
+        private void Init()
+        {
+            _readThread = new Thread(Read);
+            _readThread.IsBackground = true;
+            _readThread.Start();
+
+            _sendThread = new Thread(Send);
+            _sendThread.IsBackground = true;
+            _sendThread.Start();
+
+            _processThread = new Thread(Process);
+            _processThread.IsBackground = true;
+            _processThread.Start();
+
+            Handshake(754, 2);
+            Login("Oktay");
+        }
+
+        private void Process(object? obj)
+        {
+            while (IsConnected)
+            {
+                if (InQueue.Count > 0)
+                {
+                    try
+                    {
+                        if (InQueue.TryDequeue(out var data))
+                        {
+                            if (data == null)
+                            {
+                                continue;
+                            }
+
+                            var packet = new MemoryStream(data.Data);
+                            bool isCompressed = false;
+                            int dataLength = 0;
+
+                            if (CompressionThreshold >= 0)
+                            {
+                                if (data.PacketLength >= CompressionThreshold)
+                                {
+                                    dataLength = packet.ReadVarInt();
+                                    isCompressed = dataLength > 0;
+                                }
+                            }
+
+                            if (isCompressed)
+                            {
+                                //// Create a ZLibStream with the MemoryStream as the source stream.
+                                //ZLibStream zlibStream = new ZLibStream(new MemoryStream(data.Data), CompressionMode.Decompress);
+                                //
+                                //// Read the decompressed data from the ZLibStream.
+                                //byte[] decompressedData = new byte[zlibStream.Length];
+                                //zlibStream.Read(decompressedData, 0, decompressedData.Length);
+
+                                //using (MemoryStream compressedStream = new MemoryStream(packet.ReadBytes(10)))
+                                //{
+                                //    using (MemoryStream decompressedStream = new MemoryStream())
+                                //    {
+                                //        using (DeflateStream deflateStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+                                //        {
+                                //            deflateStream.CopyTo(decompressedStream);
+                                //        }
+                                //        Console.WriteLine("{0:x2}", decompressedStream.ReadVarInt());
+                                //    }
+                                //}
+                            }
+                            else
+                            {
+                                PacketHandler.HandlePacket(packet, this); //Uncompressed
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        IsConnected = false;
+                        _socket.Disconnect(false);
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private void Send(object? obj)
+        {
+            while (IsConnected)
+            {
+                if (OutQueue.Count > 0)
+                {
+                    if (OutQueue.TryDequeue(out var packet))
+                    {
+                        if (CompressionThreshold >= 0)
+                        {
+                            if (packet.Length >= CompressionThreshold)
+                            {
+                                // packet needs to be compressed
+                            }
+                        }
+                        SendDataAsync(packet);
+                        //packet.Dispose();
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private void Read(object? obj)
+        {
+            while (IsConnected)
+            {
+                if (_networkStream.DataAvailable)
+                {
+                    
+                    int packetLength = _networkStream.ReadVarInt();
+
+                    Console.WriteLine(packetLength);
+                    
+                    if (CompressionThreshold >= 0)
+                    {
+                        if (packetLength >= CompressionThreshold)
+                        {
+                            // packet is compressed
+                        }
+                    }
+
+                    ReceiveData(packetLength);
+                }
+                else
+                {
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private void SetPosition(double x, double y, double z, float yaw, float pitch)
+        {
+            Player.X = x;
+            Player.Y = y;
+            Player.Z = z;
+            Player.Yaw = yaw;
+            Player.Pitch = pitch;
+        }
+
+
+        public async Task SendDataAsync(byte[] data)
+        {
+            await _networkStream.WriteAsync(data, 0, data.Length);
+        }
+
+        public async Task SendDataAsync(MemoryStream memoryStream)
+        {
+            if (memoryStream != null)
+            {
+                byte[] data = memoryStream.ToArray();
+                await _networkStream.WriteAsync(data, 0, data.Length);
+            }
+        }
+
+        public async Task<byte[]> ReceiveDataAsync(int bufferSize)
+        {
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = await _networkStream.ReadAsync(buffer, 0, bufferSize);
+            if (bytesRead > 0)
+            {
+                byte[] receivedData = new byte[bytesRead];
+                Array.Copy(buffer, receivedData, bytesRead);
+                InQueue.Enqueue(new PacketStream()
+                {
+                    Data = receivedData,
+                    PacketLength = bufferSize
+                });
+                return receivedData;
+            }
+            return null;
+        }
+
+        public byte[] ReceiveData(int bufferSize)
+        {
+            byte[] buffer = new byte[bufferSize];
+            int totalBytesRead = 0;
+
+            while (totalBytesRead < bufferSize)
+            {
+                int bytesRead = _networkStream.Read(buffer, totalBytesRead, bufferSize - totalBytesRead);
+
+                if (bytesRead == 0)
+                {
+                    // Wenn Read() 0 Bytes zurückgibt, bedeutet dies, dass die Verbindung geschlossen wurde.
+                    throw new IOException("Verbindung geschlossen, bevor genügend Daten empfangen wurden.");
+                }
+
+                totalBytesRead += bytesRead;
+            }
+
+            return buffer;
+        }
+
+
+        private void Handshake(int protocolVersion, int nextStep = 1)
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x00);
+            stream.WriteVarInt(protocolVersion);
+            stream.WriteString(_address);
+            stream.WriteUnsignedShort(_port);
+            stream.WriteVarInt(nextStep);
+            OutQueue.Enqueue(stream.ToPacket());
+        }
+
+        private void Login(string name = "Deus")
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x00);
+            stream.WriteString(name);
+            OutQueue.Enqueue(stream.ToPacket());
+        }
+
+       public void KeepAlive(long id)
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x10);
+            stream.WriteLong(id);
+            OutQueue.Enqueue(stream.ToPacket());
+        }
+
+        private void TeleportConfirm(int id)
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x00);
+            stream.WriteVarInt(id);
+
+            OutQueue.Enqueue(stream.ToPacket());
+        }
+
+        public void SendPosition(Player player)
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x12);
+            stream.WriteDouble(player.X);
+            stream.WriteDouble(player.Y);
+            stream.WriteDouble(player.Z);
+            stream.WriteBool(true);
+            OutQueue.Enqueue(stream.ToPacket());
+        }
+
+        public void SwingArm(int id)
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x2C);
+            stream.WriteVarInt(id);
+            OutQueue.Enqueue(stream.ToPacket());
+        }
+
+        public void Chat(string message)
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x03);
+            stream.WriteString(message);
+            OutQueue.Enqueue(stream.ToPacket());
+        }
+
+    }
+}
