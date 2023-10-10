0796437ece40039369c71a23293c754e39996ff3
SameplayerDE <asameplayer@gmail.com>
2023-10-10 14:08:17 +0200

    dawdadwada

--- Monocraft.Application.Shared/Game1.cs
+++ Monocraft.Application.Shared/Game1.cs
@@ -1,19 +1,66 @@
-﻿using Microsoft.Xna.Framework;
+﻿using ConsoleClient;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Monocraft.Application.Shared
 {
+
+    public struct Block
+    {
+        public int X, Y, Z;
+        public int Id;
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private SpriteFont _font;
+
+        NetClient client = new NetClient();
+
+        private List<Block> _blocks = new();
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            client.ConnectAsync("localhost", 25565);
+            PacketHandler.OnBlockChange += UpdateBlock;
             IsMouseVisible = true;
+        }
+
+        private void UpdateBlock(object sender, BlockChangeEventArgs e)
+        {
+
+            Console.WriteLine(e.ToString());
+
+            int x = e.X;
+            int y = e.Y;
+            int z = e.Z;
+            int id = e.Id;
+
+            for (int i = _blocks.Count - 1; i >= 0; i--)
+            {
+                Block b = _blocks[i];
+                if (b.X == x && b.Y == y && b.Z == z)
+                {
+                    _blocks.Remove(b);
+                }
+            }
+
+            _blocks.Add(new Block()
+            {
+                X = x,
+                Y = y,
+                Z = z,
+                Id = id
+            });
+
+
         }
 
         protected override void Initialize()
@@ -26,8 +73,7 @@ namespace Monocraft.Application.Shared
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            _font = Content.Load<SpriteFont>("font");
         }
 
         protected override void Update(GameTime gameTime)
@@ -44,7 +90,23 @@ namespace Monocraft.Application.Shared
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            for (int i = _blocks.Count - 1; i >= 0; i--)
+            {
+                Block block = _blocks[i];
+                int x = block.X;
+                int y = block.Y;
+                int z = block.Z;
+                int id = block.Id;
+                if (id == 0)
+                {
+                    continue;
+                }
+                _spriteBatch.DrawString(_font, $"{id}", new Vector2(x, z) * 16, Color.White);
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
