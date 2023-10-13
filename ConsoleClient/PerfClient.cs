104549cf242204cc8ecfef9707f93a248b8a8a0c
SameplayerDE <asameplayer@gmail.com>
2023-10-13 13:27:37 +0200

    dwadawdawdawdawda

---
+++ ConsoleClient/PerfClient.cs
@@ -0,0 +1,96 @@
+﻿using MonoCraft.Net;
+using MonoCraft.Net.Predefined.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    public class PerfClient
+    {
+
+        private TcpClient _client;
+        private Thread _readThread;
+        private string _address;
+        private ushort _port;
+
+        public bool IsConnected => _client.Connected;
+
+        public PerfClient()
+        {
+            _client = new TcpClient();
+        }
+
+        public void Connect(string address, ushort port)
+        {
+            _address = address;
+            _port = port;
+            try
+            {
+                _client.Connect(address, port);
+
+                if (_client.Connected)
+                {
+
+                    Handshake((int)MinecraftVersion.Ver_1_16_4, 2);
+                    Login("Oktay");
+
+                    _readThread = new Thread(Read);
+                    _readThread.IsBackground = true;
+                    _readThread.Start();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private async void Read(object? obj)
+        {
+            while (_client.Connected)
+            {
+                if (_client.Available > 10)
+                {
+                    int available = _client.Available;
+                    byte[] buffer = new byte[available];
+                    _client.GetStream().Read(buffer, 0, available);
+                    foreach (byte b in buffer)
+                    {
+                        Console.WriteLine($"{Convert.ToString(b, 2)}");
+                    }
+                }
+                else
+                {
+                    await Task.Delay(1);
+                }
+            }
+        }
+
+        public void Handshake(int protocolVersion, int nextStep = 1)
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x00);
+            stream.WriteVarInt(protocolVersion);
+            stream.WriteString(_address);
+            stream.WriteUnsignedShort(_port);
+            stream.WriteVarInt(nextStep);
+            _client.GetStream().Write(stream.ToPacket().ToArray());
+        }
+
+        public void Login(string name = "Deus")
+        {
+            var stream = new MemoryStream();
+            stream.WriteVarInt(0x00);
+            stream.WriteString(name);
+            _client.GetStream().Write(stream.ToPacket().ToArray());
+        }
+
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -2,87 +2,95 @@
 using System;
 using MonoCraft.Net.Predefined.Enums;
 
-NetClient client = new NetClient(MinecraftVersion.Ver_1_16_4);
-client.OnConnectionEstablished += Init;
-//client.OnServerTick += () => client.Chat("Tick");
-
+PerfClient client = new PerfClient ();
 client.Connect("localhost", 25565);
 
-Player Player = client.Player;
-Random random = new Random();
-bool ChangedPosition = false;
 while (client.IsConnected)
 {
-    if (Player == null)
-    {
-        Player = client.Player;
-        continue;
-    }
-
-    var input = Console.ReadLine();
-    if (string.IsNullOrEmpty(input))
-    {
-        continue;
-    }
-    var parts = input.Split(' ');
-    var command = parts[0];
-    if (string.Equals(command, "dc"))
-    {
-        await client.DisconnectAsync();
-    }
-    if (string.Equals(command, "w"))
-    {
-        Player.X++;
-        ChangedPosition = true;
-    }
-    if (string.Equals(command, "a"))
-    {
-        Player.Z++;
-        ChangedPosition = true;
-    }
-    if (string.Equals(command, "s"))
-    {
-        Player.X--;
-        ChangedPosition = true;
-    }
-    if (string.Equals(command, "d"))
-    {
-        Player.Z--;
-        ChangedPosition = true;
-    }
-    if (string.Equals(command, "chat"))
-    {
-        if (parts.Length > 1)
-        {
-            client.Chat(string.Join(" ", parts[1..]));
-        }
-    }
-    
-    if (ChangedPosition)
-    {
-        client.SendPosition(Player);
-    }
-
-    //
-    //Thread.Sleep(50);
-    //
-    //Player.X += Player.VelX;
-    //Player.Z += Player.VelZ;
-    //
-    //if (Player.X + Player.VelX >= 24 || Player.X + Player.VelX <= -7)
-    //{
-    //    Player.VelX *= -1;
-    //}
-    //if (Player.Z + Player.VelZ >= 24 || Player.Z + Player.VelZ <= -7)
-    //{
-    //    Player.VelZ *= -1;
-    //}
-    //client.SendPosition(Player);
 
 }
 
-void Init()
-{
-    Console.WriteLine("connected to the server");
-}
+//NetClient client = new NetClient(MinecraftVersion.Ver_1_16_4);
+//client.OnConnectionEstablished += Init;
+////client.OnServerTick += () => client.Chat("Tick");
+//
+//client.Connect("localhost", 25565);
+//
+//Player Player = client.Player;
+//Random random = new Random();
+//bool ChangedPosition = false;
+//while (client.IsConnected)
+//{
+//    if (Player == null)
+//    {
+//        Player = client.Player;
+//        continue;
+//    }
+//
+//    var input = Console.ReadLine();
+//    if (string.IsNullOrEmpty(input))
+//    {
+//        continue;
+//    }
+//    var parts = input.Split(' ');
+//    var command = parts[0];
+//    if (string.Equals(command, "dc"))
+//    {
+//        await client.DisconnectAsync();
+//    }
+//    if (string.Equals(command, "w"))
+//    {
+//        Player.X++;
+//        ChangedPosition = true;
+//    }
+//    if (string.Equals(command, "a"))
+//    {
+//        Player.Z++;
+//        ChangedPosition = true;
+//    }
+//    if (string.Equals(command, "s"))
+//    {
+//        Player.X--;
+//        ChangedPosition = true;
+//    }
+//    if (string.Equals(command, "d"))
+//    {
+//        Player.Z--;
+//        ChangedPosition = true;
+//    }
+//    if (string.Equals(command, "chat"))
+//    {
+//        if (parts.Length > 1)
+//        {
+//            client.Chat(string.Join(" ", parts[1..]));
+//        }
+//    }
+//    
+//    if (ChangedPosition)
+//    {
+//        client.SendPosition(Player);
+//    }
+//
+//    //
+//    //Thread.Sleep(50);
+//    //
+//    //Player.X += Player.VelX;
+//    //Player.Z += Player.VelZ;
+//    //
+//    //if (Player.X + Player.VelX >= 24 || Player.X + Player.VelX <= -7)
+//    //{
+//    //    Player.VelX *= -1;
+//    //}
+//    //if (Player.Z + Player.VelZ >= 24 || Player.Z + Player.VelZ <= -7)
+//    //{
+//    //    Player.VelZ *= -1;
+//    //}
+//    //client.SendPosition(Player);
+//
+//}
+//
+//void Init()
+//{
+//    Console.WriteLine("connected to the server");
+//}
 
